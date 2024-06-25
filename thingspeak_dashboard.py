5337fe267657304cae920bec102bbfe491147f87
PM1980 <39511663+PM1980@users.noreply.github.com>
2024-06-25 16:19:44 -0300

    Update thingspeak_dashboard.py

--- thingspeak_dashboard.py
+++ thingspeak_dashboard.py
@@ -109,7 +109,7 @@ def main():
 
     elif selected == "Contact Us":
         st.subheader(f"**Informações para contato e redes sociais**")
-        st.markdown("Instagram [here](www.instagram.com/projeto_ecivil/).")
+        st.markdown("Instagram [website](www.instagram.com/projeto_ecivil/).")
         # Contact form or information would go here
 
 if __name__ == "__main__":
